3b32392b17830915f09949f565c28249d6ae4261
AARON ISRAEL <htlgil41@gmail.com>
2026-02-26 19:25:58 -0400

    Se creo la configuracion del cliente ssh y se agrego el paquete de ssh para generar la configuracion del cliente

---
+++ config/ssgclient/client.go
@@ -0,0 +1,28 @@
+package ssgclient
+
+import (
+	"fmt"
+	"time"
+
+	"golang.org/x/crypto/ssh"
+)
+
+var configClientSsh *ssh.ClientConfig = &ssh.ClientConfig{
+	User: "aaron",
+	Auth: []ssh.AuthMethod{
+		ssh.Password("PASSWORD"),
+	},
+	HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	Timeout:         time.Second * 2,
+}
+
+func CreateClientSsh(ip string) (*ssh.Client, error) {
+
+	cliente, errCreateClient := ssh.Dial("tcp", ip, configClientSsh)
+	if errCreateClient != nil {
+
+		return nil, fmt.Errorf("Error al tratar de crear la conexion con %q en la ip %q", errCreateClient.Error(), ip)
+	}
+
+	return cliente, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module ssh-comandoker
 
 go 1.25.5
+
+require (
+	golang.org/x/crypto v0.48.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+)
